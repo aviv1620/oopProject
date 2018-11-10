58a4c78dbb5646de39446ef2b647558a297d429d
aviv <aviv1620@gmail.com>
2018-11-10 21:25:16 +0200

    add test

--- src/myMath/Test.java
+++ src/myMath/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("######chengh######");
+		System.out.println("######more chengh######");
 		System.out.println("######Comperator######");
 		testComperator();
 		
