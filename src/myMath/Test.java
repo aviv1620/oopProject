9890e184c734ef23ce4f6d9032f489880197a128
avivlen <avivlen@DESKTOP-L7TOF4E>
2018-11-14 15:29:24 +0200

    test add from another computer. add note to test

--- src/myMath/Test.java
+++ src/myMath/Test.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 public class Test {
 	//sasas
+	//test add from another computer.
 
 	public static void main(String[] args) {
 		System.out.println("######chengh######");
